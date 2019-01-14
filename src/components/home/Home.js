fba1d72898f2420d5551ed72145e9dc6ac163ad4
Danijel Buhin <danijelbuhin6@gmail.com>
2019-01-14 21:50:02 +0100

    Fix eslint fetch not defined error, fix other eslint warnings

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
     const languages = ['ruby', 'php', 'javascript', 'java', 'python', 'css'];
     languages.map(l => fetch(`https://api.github.com/search/repositories?q=topic:${l}&client_id=edc304d4e5871143c167&client_secret=39e5f4613d7c4c23e96b7ad2f0b2b7546e05fb19&sort=stars&order=desc`, {
       headers: {
-        Accept: 'application/vnd.github.mercy-preview+json'
+        Accept: 'application/vnd.github.mercy-preview+json',
       },
     })
       .then(res => res.json())
@@ -25,12 +25,11 @@ class Home extends Component {
         this.setState(prevState => ({
           popularRepos: [...prevState.popularRepos, ...topFive],
         }));
-      }),
-    );
+      }));
   }
 
   render() {
-    const sorted = this.state.popularRepos.sort((a, b) => b.stargazers_count - a.stargazers_count)
+    const sorted = this.state.popularRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
     console.log(sorted);
     return (
       <div>Home</div>

4cb300034ffaeda11d352d1af8a952120a25ee04
Danijel Buhin <danijelbuhin6@gmail.com>
2019-01-15 11:03:35 +0100

    Add axios, refactor fetching popular repos functionality

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,37 +1,56 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import withAppContext from '../shared/app/withAppContext';
 
 class Home extends Component {
   state = {
     popularRepos: [],
+    isLoading: true,
   }
 
   componentDidMount() {
-    this.fetchPopularRepos();
+    const languages = ['ruby', 'php', 'javascript', 'java', 'python'];
+    const promises = [];
+    languages.forEach((l) => {
+      promises.push(this.fetchPopularRepos(l));
+    });
+    axios.all(promises).then(() => {
+      this.setState(({ popularRepos }) => ({
+        isLoading: false,
+        popularRepos: popularRepos.sort((a, b) => b.stargazers_count - a.stargazers_count),
+      }));
+    });
   }
 
-  fetchPopularRepos() {
-    const languages = ['ruby', 'php', 'javascript', 'java', 'python', 'css'];
-    languages.map(l => fetch(`https://api.github.com/search/repositories?q=topic:${l}&client_id=edc304d4e5871143c167&client_secret=39e5f4613d7c4c23e96b7ad2f0b2b7546e05fb19&sort=stars&order=desc`, {
-      headers: {
-        Accept: 'application/vnd.github.mercy-preview+json',
-      },
-    })
-      .then(res => res.json())
-      .then((body) => {
-        const topFive = body.items.filter((_, i) => i < 5);
+  fetchPopularRepos(language) {
+    return axios
+      .get(`https://api.github.com/search/repositories?q=topic:${language}&client_id=edc304d4e5871143c167&client_secret=39e5f4613d7c4c23e96b7ad2f0b2b7546e05fb19&sort=stars&order=desc`, {
+        headers: {
+          Accept: 'application/vnd.github.mercy-preview+json',
+        },
+      })
+      .then(({ data }) => {
+        const topFive = data.items.filter(item => item.language).filter((_, i) => i < 5);
         this.setState(prevState => ({
           popularRepos: [...prevState.popularRepos, ...topFive],
         }));
-      }));
+      });
   }
 
   render() {
-    const sorted = this.state.popularRepos.sort((a, b) => b.stargazers_count - a.stargazers_count);
-    console.log(sorted);
+    const { isLoading, popularRepos } = this.state;
+    if (isLoading) {
+      return <div>Loading..</div>;
+    }
     return (
-      <div>Home</div>
+      <div>
+        {popularRepos.map(repo => (
+          <div key={repo.name} style={{ borderBottom: '1px solid black' }}>
+            {repo.name} - {repo.stargazers_count} - {repo.language}
+          </div>
+        ))}
+      </div>
     );
   }
 }

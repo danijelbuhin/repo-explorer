6056ff58aad564ad374320167eff66488be500f5
Danijel Buhin <danijelbuhin6@gmail.com>
2019-02-06 15:46:14 +0100

    Fetch repos with similar topic on repo profile page

--- src/components/repo-profile/RepoProfile.js
+++ src/components/repo-profile/RepoProfile.js
@@ -2,11 +2,38 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import axios from 'axios';
+import styled from 'styled-components';
 
 import db from '../../services/firebase';
 
 import Loader from '../shared/loader/Loader';
 import withAppContext from '../shared/app/withAppContext';
+
+import { ReactComponent as StarSVG } from '../home/assets/Star.svg';
+
+import Repo, { Wrapper as RepoWrapper } from '../shared/repo/Repo';
+
+const RepoList = styled.div`
+  display: flex;
+  align-items: stretch;
+  flex-wrap: wrap;
+
+  ${RepoWrapper} {
+    width: 100%;
+    @media (min-width: 420px) {
+      width: 48%;
+    }
+    @media (min-width: 768px) {
+      width: 31%;
+    }
+    @media (min-width: 900px) {
+      width: 23%;
+    }
+    @media (min-width: 1200px) {
+      width: 18%;
+    }
+  }
+`;
 
 const views = db.collection('views');
 
@@ -45,12 +72,16 @@ const handleView = (params) => {
 };
 
 const RepoProfile = (props) => {
-  const { match: { params: { id } } } = props;
+  const { match: { params: { id } }, appContext } = props;
 
   const [isLoading, setIsLoading] = useState(true);
   const [repo, setRepo] = useState({});
 
+  const [similarRepos, setSimilarRepos] = useState([]);
+  const [isLoadingSimilarRepos, setIsLoadingSimilarRepos] = useState(true);
+
   useEffect(() => {
+    document.title = `Fetching ${decodeURIComponent(id)} | Repo Explorer`;
     props.appContext.fetchRepo(decodeURIComponent(id)).then((data) => {
       document.title = `Exploring ${data.full_name} | Repo Explorer`;
       const viewedData = {
@@ -63,8 +94,12 @@ const RepoProfile = (props) => {
       handleView(viewedData);
       setIsLoading(false);
       setRepo(data);
+      appContext.searchRepo(data.topics[0], 5).then(({ items }) => {
+        setSimilarRepos(items);
+        setIsLoadingSimilarRepos(false);
+      });
     });
-  }, []);
+  }, [id]);
 
   if (isLoading) {
     return <Loader text={`Fetching information about ${decodeURIComponent(id)}`} />;
@@ -73,6 +108,22 @@ const RepoProfile = (props) => {
   return (
     <div>
       {repo.full_name}
+      <div>Repos with similar topic:</div>
+      {isLoadingSimilarRepos && 'Loading similar repos....'}
+      <RepoList>
+        {!isLoadingSimilarRepos && similarRepos.length > 0 && similarRepos.map(similar => (
+          <Repo
+            key={similar.id}
+            avatar={similar.owner.avatar_url}
+            name={similar.full_name}
+            count={similar.stargazers_count}
+            countIcon={<StarSVG />}
+            language={similar.language}
+            topic={similar.topics[0]}
+            id={similar.id}
+          />
+        ))}
+      </RepoList>
     </div>
   );
 };
